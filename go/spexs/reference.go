201e8f4da64e46f51b1bdb8f181e05e0994bc7e3
egon <egon.elbre@raintree.ee>
2012-01-30 19:50:25 +0200

    removed SeekFrom

--- go/spexs/reference.go
+++ go/spexs/reference.go
@@ -24,15 +24,3 @@ func (ref *UnicodeReferences) Next(p Pos) (Char, Pos, bool) {
   rune, width := utf8.DecodeRune( ref.Pats[idx][pos:] )
   return rune, p + width, true
 }
-
-func (ref *UnicodeReferences) SeekFrom(p Pos, amount int) (Char, Pos, bool) {
-  idx, pos := PosDecode(p)
-
-  /* TODO: seek properly utf8 style */
-  if p + amount >= ref.Pats[idx].Length {
-  	return 0, EmptyPos, false
-  }
-
-  rune := ref.Pats[idx][ pos + amount ]
-  return rune, p + amount, true
-}
