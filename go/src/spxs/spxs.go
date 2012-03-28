b37cf97ceccab0abd5a9f6e4fccc56f48e433d3e
egon <egon.elbre@raintree.ee>
2012-03-28 13:55:24 +0300

    move data files, added yeast data
    Former-commit-id: d81a1740069a6680bbe8f57408e75f390837bdd7

--- go/src/spxs/spxs.go
+++ go/src/spxs/spxs.go
@@ -12,6 +12,32 @@ import (
 	"log"
 )
 
+/*
+	multiple limiters
+	output limiters
+	p-value binom/hyper
+
+	flexibility wildcards
+	[-max_gap_nr nr]		- How many flexible gaps at most
+ 	[-min_gap nr] 			- minimum length of a gap
+ 	[-max_gap nr] 			- maximum length of a gap
+ 	[-no_gap_len nr] 		- require at least so many positions gap-less
+ 	[-init_gap_len nr] 		- Initiate that value (can require longer/shorter first motif...)
+ 	[-only_print_if_gap_allowed]	- only report motif if gap could be allowed at that pos
+	
+	output ===
+	-length:6..
+	-count:10..
+
+	fitness ===
+	-p-value: -1
+
+
+	/group/inbox/elbre
+
+ 	// -acceptable
+*/
+
 var (
     characterFile *string = flag.String("chars", "", "character set file")
     referenceFile *string = flag.String("ref", "", "reference file")   
--- go/test.sh
+++ go/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export GOPATH=`pwd`
 go build spxs
-time spxs --ref=dna.gen --chars=dna.set --extender=regexp --fitness=complexity --limit=4000 --procs=8
+time spxs --ref=data/dna.gen --chars=data/dna.set --extender=regexp --fitness=complexity --limit=4000 --procs=8
