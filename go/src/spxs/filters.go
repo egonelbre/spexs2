6ed325ecd6970ca6d83f858e1d7eed96efc365ae
egon <egon.elbre@raintree.ee>
2012-04-04 17:43:04 +0300

    composite filter and filter creation
    Former-commit-id: cb71cf42d8b9cd62b740cb76b9c8763186251ccf

--- go/src/spxs/filters.go
+++ go/src/spxs/filters.go
@@ -1,27 +1,97 @@
 package main
 
-import . "spexs"
+import (
+	. "spexs"
+	"math"
+	"log"
+)
 
-type TrieFilterCreator func(interface{}) TrieFilterFunc
+type TrieFilterCreator func(interface{}, Setup) TrieFilterFunc
 
-var filters = map[string] PatternFilterCreator {
-	"count": func(limit int) TrieFilterFunc {
-		return func(p *TrieNode) bool {
-			return p.Pos.Len() >= limit
-		}
-	},
-	"length": func(limit int) TrieFilterFunc {
-		return func(p *TrieNode) bool {
-			return p.Len() <= limit
-		}
-	},
-	"complexity": func(limit int) TrieFilterFunc {
-		return func(p *TrieNode) bool {
-			return p.Complexity() <= limit
-		}
-	},
+type GenericFilterConf struct { Min, Max float64 }
+
+func TrueFilter(p *TrieNode) bool {
+	return true
+}
+
+var Filters = map[string] TrieFilterCreator {
+	"count": CountFilterCreator,
+	"length": LengthFilterCreator,
+	"p-value": PValueFilterCreator,
 }
 
 func CreateFilter(conf map[string]interface{}, setup Setup) TrieFilterFunc {
+	filters := make([]TrieFilterFunc, 1)
+
+	for name, args := range conf {
+		if _, valid := Filters[name]; !valid {
+			log.Fatal("No filter named: ", name)
+		}
+
+		f := Filters[name](args, setup)
+		filters = append(filters, f)
+	}
+
+	switch len(filters) {
+	case 0 : return TrueFilter
+	case 1 : return filters[0]
+	}
 	
+	// create a composite filter
+	return func(p *TrieNode) bool {
+		for _, f := range filters {
+			if !f(p) {
+				return false
+			}
+		}
+		return true
+	}	
+}
+
+type TrieValueFunc func(p *TrieNode) float64
+
+func GenericFilter(value TrieValueFunc, config interface{}) TrieFilterFunc {
+	var conf GenericFilterConf
+	conf.Min = math.NaN()
+	conf.Max = math.NaN()
+
+	ApplyObject(config, conf)
+
+	min, max := conf.Min, conf.Max
+	low, high := !math.IsNaN(conf.Min), !math.IsNaN(conf.Max)
+	
+	if low && high {
+		return func(p *TrieNode) bool {
+			return (value(p) <= max) && (value(p) > min)
+		}
+	} else if low {
+		return func(p *TrieNode) bool {
+			return value(p) > min
+		}
+	} else if high {
+		return func(p *TrieNode) bool {
+			return value(p) <= max
+		}
+	}
+
+	log.Fatal("Neither min or max was defined for count filter.")
+	return TrueFilter
+}
+
+func CountFilterCreator(conf interface{}, setup Setup) TrieFilterFunc {
+	return GenericFilter(func(p *TrieNode) float64{
+		return float64(p.Pos.Len())
+	}, conf)
+}
+
+func LengthFilterCreator(conf interface{}, setup Setup) TrieFilterFunc {	
+	return GenericFilter(func(p *TrieNode) float64{
+		return float64(p.Pos.Len())
+	}, conf)
+}
+
+func PValueFilterCreator(conf interface{}, setup Setup) TrieFilterFunc {
+	return GenericFilter(func(p *TrieNode) float64{
+		return p.PValue(setup.Ref)
+	}, conf)
 }
