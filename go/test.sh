5313b0d95e4ab8b29f24dc1b2b16a81e45a0b79c
egon <egon.elbre@raintree.ee>
2012-04-11 14:22:52 +0300

    idea: new conf file style
    Former-commit-id: 26bcf49d512cb22c623c568845f30e4a461e3ecb

--- go/test.sh
+++ go/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export GOPATH=`pwd`
 go build spxs
-time spxs --ref=data/dna.gen --chars=data/dna.set --extender=regexp --fitness=complexity --limit=4000 --procs=8
+time spxs ref=data/dna.gen
