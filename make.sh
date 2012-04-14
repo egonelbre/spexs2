a7af3ebed1c152f1dfc097cf66730db901835176
egon <egon.elbre@raintree.ee>
2012-04-14 11:37:13 +0300

    added explicit bin directory
    Former-commit-id: 0feb48ee85c8553ecfd9d2d10e18ef1e3d6ef57c

--- make.sh
+++ make.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
 export GOPATH=`pwd`
+export GOBIN=`pwd`/bin
 
 go install spexs
 go install spxs
--- test.sh
+++ test.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 export GOPATH=`pwd`
+export GOBIN=`pwd`/bin
+
+go install spxs
 
 time ./bin/spxs --conf=conf/spxs.json ref=data/dna.gen
