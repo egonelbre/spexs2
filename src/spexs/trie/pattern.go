8b07908a5f6a9c685ac4a34d08ce2844dbbc197c
egon <egon.elbre@raintree.ee>
2012-04-16 20:23:36 +0300

    fix: patterns
    Former-commit-id: 2ab0dc17ac6b7d9cc2813dbd911b92c5bff4866b

---
+++ src/spexs/trie/pattern.go
@@ -0,0 +1,92 @@
+package trie
+
+import (
+	"spexs"
+	"stats"
+)
+
+type Pattern struct {
+	Char       Char
+	Parent     *Pattern
+	Pos        spexs.Set
+	IsGroup    bool
+	IsStar     bool
+	length     int
+	complexity int
+	pvalue     float64
+}
+
+func NewPattern(char Char, parent *Pattern) *Pattern {
+	p := &Pattern{}
+	p.Char = char
+	p.Parent = parent
+	if parent != nil {
+		p.Pos = spexs.NewHashSet(parent.Pos.Len() / 2)
+	} else {
+		p.Pos = spexs.NewHashSet(0)
+	}
+	p.IsGroup = false
+	p.IsStar = false
+	p.length = -1
+	p.complexity = -1
+	p.pvalue = -1
+	return p
+}
+
+func NewFullPattern(ref *Reference) *Pattern {
+	p := NewPattern(0, nil)
+	p.Pos = NewFullSet(ref)
+	return p
+}
+
+func (n *Pattern) String() string {
+	if n.Parent != nil {
+		if n.IsStar {
+			return n.Parent.String() + string('*') + string(n.Char)
+		} else {
+			return n.Parent.String() + string(n.Char)
+		}
+	}
+	return ""
+}
+
+func (n *Pattern) Len() int {
+	if n.Parent != nil {
+		if n.length < 0 {
+			n.length = n.Parent.Len() + 1
+		}
+		return n.length
+	}
+	return 0
+}
+
+func (n *Pattern) Complexity() int {
+	if n.Parent != nil {
+		if n.complexity < 0 {
+			if n.IsStar {
+				n.complexity = n.Parent.Complexity() + 4
+			} else if n.IsGroup {
+				n.complexity = n.Parent.Complexity() + 2
+			} else {
+				n.complexity = n.Parent.Complexity() + 1
+			}
+		}
+		return n.complexity
+	}
+	return 0
+}
+
+func (n *Pattern) PValue(ref Reference) float64 {
+	if n.pvalue >= 0 {
+		return n.pvalue
+	}
+
+	counts := make([]int, len(ref.Groupings))
+
+	for idx := range n.Pos.Iter() {
+		counts[ref.Pats[idx].Group] += 1
+	}
+
+	n.pvalue = stats.HypergeometricSplitLog(counts[0], counts[1], ref.Groupings[0], ref.Groupings[1])
+	return n.pvalue
+}
---
+++ src/spexs/trie/pattern_test.go
@@ -0,0 +1,45 @@
+package trie
+
+import (
+	"testing"
+	"unicode/utf8"
+)
+
+func pat(s string) *Pattern {
+	r := NewPattern(0, nil)
+	return add(r, s)
+}
+
+func add(base *Pattern, s string) *Pattern {
+	if len(s) <= 0 {
+		return base
+	}
+	rune, size := utf8.DecodeRuneInString(s)
+	n := NewPattern(Char(rune), base)
+	if rune == 'X' {
+		n.IsStar = true
+	}
+	return add(n, s[size:])
+}
+
+func TestTrieNodeString(t *testing.T) {
+	root := pat("")
+	
+	a := add(root, "hello")
+	b := add(a, " world")
+	c := add(root, "hello mXgic")
+	d := add(root, "testing")
+	e := add(d, " heist")
+
+	test := func(n *Pattern, s string) {
+		if n.String() != s {
+			t.Errorf("wrong result got='%s' expected='%s'", n.String(), s)
+		}
+	}
+
+	test(a, "hello")
+	test(b, "hello world")
+	test(c, "hello m*Xgic")
+	test(d, "testing")
+	test(e, "testing heist")
+}
