1b573e42a43b7a7cfe52b5f11246a12869458ec8
egon <egon.elbre@raintree.ee>
2012-04-27 13:41:37 +0300

    dev: better version handling
    Former-commit-id: 37972ee181d50b0abea767c2fa5824027e7981eb

--- make.sh
+++ make.sh
@@ -2,9 +2,10 @@
 export GOPATH=`pwd`
 export GOBIN=`pwd`/bin
 
-TIME=`date  +%D\\ %H:%M:%S`
-SHA=`git log --pretty=format:'%h' -n 1`
-echo -e "package main\n\nconst theVersion = \"v0.2 + $TIME g.$SHA\"" > src/spxs/version.go
+REVS=`git log --oneline | wc -l | sed "s/ //g"`
+VERSION=`git describe --tags --long`
+TIME=`date +%D\\ %H:%M:%S`
+echo -e "package main\n\nconst theVersion = \"$VERSION-rev$REVS\"" > src/spxs/version.go
 
 go install spexs
 go install spxs
