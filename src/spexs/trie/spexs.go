94e23e8bbc0e29f46c4a0b7964c500c4fb8259a0
egon <egon.elbre@raintree.ee>
2012-04-27 18:27:38 +0300

    testing parallelization
    Former-commit-id: 49142308b36685fe4f8a683530a327ac80efe781

--- src/spexs/trie/spexs.go
+++ src/spexs/trie/spexs.go
@@ -62,16 +62,48 @@ func Run(s *Setup) {
 func Parallel(f func(), routines int) {
 	stop := make(chan int, routines)
 	for i := 0; i < routines; i += 1 {
-		go func() {
+		go func(rtn int) {
 			defer func() { stop <- 1 }()
 			f()
-		}()
+		}(i)
 	}
 	for i := 0; i < routines; i += 1 {
 		<-stop
 	}
 }
 
-func RunParallel(s *Setup, routines int) {
+func RunParallel2(s *Setup, routines int) {
 	Parallel(func() { Run(s) }, routines)
+}
+
+func RunParallel(s *Setup, routines int) {
+	start := 0
+	stop := make(chan int)
+
+	p, valid := s.In.Take()
+	if !valid {
+		return
+	}
+	extensions := s.Extender(p, s.Ref)
+	for extended := range extensions {
+		if s.Extendable(extended, s.Ref) {
+			s.In.Put(extended)
+			start += 1
+			go func(){
+				RunParallel(s, routines)
+				stop <- 1
+			}()
+		}
+		if s.Outputtable(extended, s.Ref) {
+			s.Out.Put(extended)
+		}
+	}
+
+	//if s.PostProcess(p, s) != nil {
+		//break
+	//}
+
+	for i := 0; i < start; i += 1 {
+		<-stop
+	}
 }
