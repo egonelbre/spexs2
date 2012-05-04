6a85775b2f0b1347fe8e86568a686904434021fd
egon <egon.elbre@raintree.ee>
2012-05-04 20:54:59 +0300

    refactor
    Former-commit-id: 379b39a0edea3d708497beda8632446bce91225c

--- src/spexs/pool/priority.go
+++ src/spexs/pool/priority.go
@@ -1,23 +1,23 @@
 package pool
 
 import (
-	"spexs"
+	. "spexs"
 	"container/heap"
 )
 
 type Priority struct {
 	token     chan int
-	items     []*spexs.Pattern
-	Fitness   spexs.FitnessFunc
+	items     []*Pattern
+	Fitness   FitnessFunc
 	length    int
 	limit     int
 	ascending bool
 }
 
-func NewPriority(fitness spexs.FitnessFunc, limit int, ascending bool) *Priority {
+func NewPriority(fitness FitnessFunc, limit int, ascending bool) *Priority {
 	p := &Priority{}
 	p.token = make(chan int, 1)
-	p.items = make([]*spexs.Pattern, limit+100)
+	p.items = make([]*Pattern, limit+100)
 	p.length = 0
 	p.limit = limit
 	p.Fitness = fitness
@@ -32,7 +32,7 @@ func (p *Priority) IsEmpty() bool {
 	return p.length == 0
 }
 
-func (p *Priority) Take() (*spexs.Pattern, bool) {
+func (p *Priority) Take() (*Pattern, bool) {
 	<-p.token
 	if p.IsEmpty() {
 		p.token <- 1
@@ -40,10 +40,10 @@ func (p *Priority) Take() (*spexs.Pattern, bool) {
 	}
 	v := heap.Pop(p)
 	p.token <- 1
-	return v.(*spexs.Pattern), true
+	return v.(*Pattern), true
 }
 
-func (p *Priority) Put(pat *spexs.Pattern) {
+func (p *Priority) Put(pat *Pattern) {
 	<-p.token
 	heap.Push(p, pat)
 	if p.limit > 0 && p.Len() > p.limit {
@@ -71,12 +71,12 @@ func (p *Priority) Less(i, j int) bool {
 // heap.Interface
 func (p *Priority) Push(x interface{}) {
 	if p.length + 1 > len(p.items) {
-		tmp := make([]*spexs.Pattern, len(p.items) + 1000)
+		tmp := make([]*Pattern, len(p.items) + 1000)
 		copy(tmp, p.items)
 		p.items = tmp
 	}
 
-	p.items[p.length] = x.(*spexs.Pattern)
+	p.items[p.length] = x.(*Pattern)
 	p.length += 1
 }
 
