daeca40d8ddae1c51f17488de0945f92557d4f85
Egon Elbre <egonelbre@gmail.com>
2012-09-03 13:48:03 +0300

    dev
    Former-commit-id: 880b8c98ddc3b3f3a51a255ed998046620bc4b41 [formerly defc39116ffded0a1230d71358d1b7fd5e13f958]
    Former-commit-id: 01bb8d4c79ed01a908cca72695480030262bb613

--- src/spxs/debugger.go
+++ src/spxs/debugger.go
@@ -11,18 +11,18 @@ var dbg = debugger.New()
 func attachDebugger(s *AppSetup) {
 	debugger.RunShell(dbg)
 	f := s.Extender
-	s.Extender = func(p *Query, ref *Database) Patterns {
-		tmp := f(p, ref)
+	s.Extender = func(q *Query, db *Database) Patterns {
+		tmp := f(q, db)
 		result := NewPatterns()
 		dbg.Break(func() {
-			fmt.Fprintf(dbg.Logout, "extending: %v\n", p.String())
+			fmt.Fprintf(dbg.Logout, "extending: %v\n", q.ToString(db))
 			for extended := range tmp {
 				result <- extended
-				fmt.Fprintf(dbg.Logout, "  | %v\n", ref.ReplaceGroups(extended.String()))
+				fmt.Fprintf(dbg.Logout, "  | %v\n", q.ToString(db))
 				if *verbose {
 					fmt.Fprintf(dbg.Logout, "      E:%v  O:%v\n", s.Extendable(extended, ref), s.Outputtable(extended, ref))
 					fmt.Fprintf(dbg.Logout, "      ")
-					s.Printer(dbg.Logout, extended, ref)
+					s.Printer(dbg.Logout, extended, db)
 				}
 			}
 			close(result)
