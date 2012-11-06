5baa5cafd70c692f3849040445cbcd26afbfaeb2
Egon Elbre <egonelbre@gmail.com>
2012-11-06 13:50:51 +0200

    dev: base for new conf
    Former-commit-id: ebdc8ab0de1f00a8d7efaebeb70054e5e4c7838e [formerly dd1e2dd5daece0f30bea3de537c0e970fa345320]
    Former-commit-id: 3dc45f4e255d3b8f75798b5188691a1f970dc224

--- src/spexs2/conf.go
+++ src/spexs2/conf.go
@@ -11,58 +11,54 @@ import (
 )
 
 const baseConfiguration = `{
-	"data" : {},
-	"alphabet" : {
-		"separator" : "",
-		"groups" : {}
+	"Dataset" : {},
+	"Reader" : {
+		"Method" : "Delimited",
+		"Separator" : ""
 	},
-	"extension" : {
-		"method" : "",
-		"args" : {},
-		"filter" : {}
+	"Extension" : {
+		"Method" : "",
+		"Groups" : {},
+		"Extendable" : {},
+		"Outputtable" : {}
 	},
-	"output" : {
-		"order" : "",
-		"sort" : "desc",
-		"filter" : {},
-		"queue" : ""
+	"Output" : {
+		"Sort" : "desc",
+		"Count" : -1
 	},
-	"print" : {
-		"count" : -1,
-		"showheader" : true,
-		"header" : "",
-		"format" : ""
-	},
-	"aliases" : {}
+	"Print" : {
+		"ShowHeader" : true
+	}
 }`
 
 type FileGroup struct {
 	File     string
 	Files    []string
 	FileList string
-
-	CountSeparator string
 }
 
 type Conf struct {
-	Data     map[string]FileGroup
-	Alphabet struct {
-		Separator string
-		Groups    map[string]struct{ Elements string }
+	Dataset map[string]FileGroup
+	Reader  struct {
+		Method         string
+		Separator      string
+		CountSeparator string
 	}
 	Extension struct {
 		Method string
-		Args   json.RawMessage
-		Filter map[string]json.RawMessage
+		Groups map[string]struct{ Elements string }
+
+		Extendable  map[string]json.RawMessage
+		Outputtable map[string]json.RawMessage
 	}
 	Output struct {
+		Method string
+		SortBy string
 		Order  string
-		Sort   string
-		Filter map[string]json.RawMessage
-		Queue  string
+		Count  int
 	}
-	Print struct {
-		Count      int
+	Printer struct {
+		Method     string
 		ShowHeader bool
 		Header     string
 		Format     string
