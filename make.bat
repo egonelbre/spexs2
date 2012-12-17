ee542cac95d959d009c343468de08fd544a23c0c
Egon Elbre <egonelbre@gmail.com>
2012-12-17 18:15:21 +0200

    added bat for building on windows
    Former-commit-id: 2128ffd078fc6171e1d060bc4b9580724f07897d [formerly 738e980fbbfa871c6f17da2b6c7db52110d7f49b]
    Former-commit-id: 2c8c9d8937d510a8214c7b7c0a54474e199095da

---
+++ make.bat
@@ -0,0 +1,5 @@
+set GOPATH=%CD%
+set GOBIN=%CD%/bin
+
+go build spexs
+go build -o bin\spexs2.exe spexs2
--- src/spexs2/version.go
+++ src/spexs2/version.go
@@ -1,6 +1,6 @@
 package main
 
 const (
-	theVersion = "1.0.0-0-g220e95f-rev303"
-	theBuildTime="12/11/12 18:38:33"
+	theVersion   = "development"
+	theBuildTime = "development"
 )
